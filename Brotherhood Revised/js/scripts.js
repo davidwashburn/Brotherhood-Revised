f148d69a1be28b7796649ecc7e25b87ae6e15ba7
David Washburn <davidwashburn@outlook.com>
2016-09-22 00:17:30 -0500

    Main Website Layout 1.0
    Cleaner. Faster. Better.

---
+++ Brotherhood Revised/js/scripts.js
@@ -0,0 +1,30 @@
+// Created by Dwavid on 9.19.16
+
+// Main Page Visual Javascript [JQERY]
+
+$( document ).ready(function() {
+    //Open Main Menu and activate dimmer
+    $('#navMenu').click('click', function (){
+        $('#mainMenu').toggleClass("active");
+        $('#overlay').toggleClass("active");
+        $('#body').toggleClass("hideOverFlow");
+    });
+    $('#menuCloseButton').click('click', function (){
+        $('#mainMenu').toggleClass("active");
+        $('#overlay').toggleClass("active");
+        $('#body').toggleClass("hideOverFlow")
+    });
+    //Click off Menu to close
+    $('#overlay').click('click', function (){
+        $('#mainMenu').toggleClass("active");
+        $('#overlay').toggleClass("active");
+        $('#body').toggleClass("hideOverFlow");
+    });
+
+    // Show/Hide Sections using slideToggle
+    $('#showHideBattle').click('click', function (){
+        $('#battle').slideToggle("fast");
+    });
+
+});
+
