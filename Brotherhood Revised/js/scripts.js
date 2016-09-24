948f3d7aa148ddc57b2f1ea6a878de00e08127db
David Washburn <davidwashburn@outlook.com>
2016-09-24 01:16:56 -0500

    Index Changes and Design Layout
    Working on the design. It's coming along much faster than the old site.
    I have  much better clue what I am doing and things are more uniformed.
    
    To Do:
    Main menu > Settings seems like a slow transition. There appears to be
    jank, but I can't figure out why. There's very little CSS involved.

--- Brotherhood Revised/js/scripts.js
+++ Brotherhood Revised/js/scripts.js
@@ -6,6 +6,7 @@ $( document ).ready(function() {
     //Open Main Menu and activate dimmer
     $('#navMenu').click('click', function (){
         $('#mainMenu').toggleClass("active");
+        $('#settingsMenu').toggleClass("active");
         $('#overlay').toggleClass("active");
         $('#body').toggleClass("hideOverFlow");
     });
@@ -16,9 +17,19 @@ $( document ).ready(function() {
     });
     //Click off Menu to close
     $('#overlay').click('click', function (){
-        $('#mainMenu').toggleClass("active");
+        $('#mainMenu').removeClass("active push");
+        $('#settingsMenu').removeClass("active push");
         $('#overlay').toggleClass("active");
         $('#body').toggleClass("hideOverFlow");
+    });
+    //Settings Menu Open and Close
+    $('#settingsMenuButton').click('click', function (){
+        $('#settingsMenu').addClass("push");
+        $('#mainMenu').addClass("push");
+    });
+    $('#settingsCloseButton').click('click', function (){
+        $('#settingsMenu').removeClass("push");
+        $('#mainMenu').removeClass("push");
     });
 
     // Show/Hide Sections using slideToggle
