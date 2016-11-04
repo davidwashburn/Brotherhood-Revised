61f25bb17fb40023fb870324b75eb1433f327f26
David Washburn <davidwashburn@outlook.com>
2016-11-04 17:33:48 -0500

    Twitter Implementation - In Progress
    Working on it. Not functional just yet.

---
+++ Brotherhood Revised/Tweetie-master/api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'jWtBBD3r82IKvCIWT5wd3SPp0');
+    define('CONSUMER_SECRET', 'A6teF5xrKttHQ08BGgqpuLswKebYgCHhKJ0wjz5hNYD5HdJf3k');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '	788766800204869633-04ZQOnT925gwj6xrx4BtwCZSuDiRPxn');
+    define('ACCESS_SECRET', 'wyovCAdZCcpr4IXSet9PcpjfQA4grQJKR9SODlxtmm7za');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
---
+++ Brotherhood Revised/php/twitter.class.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once __DIR__ . '/php/oauth.php';
+require_once __DIR__ . '/php/twitter.setup.php';
